298dca71f9ddd65ee430fc1c3af89ec3281a996f
Kirsten Netter <kirsten.netter@gmx.de>
2022-05-31 17:05:13 +0200

    initialize hosting with heroku

--- index.js
+++ index.js
@@ -338,6 +338,8 @@ app.use((err, req, res, next) => {
     res.status(500).send("Something broke!");
   });
 
-app.listen(8080, () => {
-  console.log("Your app is listening on port 8080.");
+const port = process.env.PORT || 8080;
+app.listen(port, "0.0.0.0",() => {
+  console.log("Listening on Port " + port);
 });
+
