1a5a41e90f2dd015537d8e6b44985893cdd06a8f
Kirsten <kirsten.netter@gmx.de>
2022-08-18 09:21:15 +0200

    added allowed origin localhost:4200

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const cors = require("cors");
 
 let allowedOrigins = [
     'http://localhost:8080', 
+    'http://localhost:4200',
     'http://testsite.com', 
     'http://localhost:1234', 
     'https://kirnetsmoviesapp.netlify.app/'
