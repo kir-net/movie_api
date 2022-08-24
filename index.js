8e65b09da75ea8b0502264111d37d953ae379a0f
Kirsten <kirsten.netter@gmx.de>
2022-08-24 08:56:57 +0200

    modified 'put Users/username' = edit user

--- index.js
+++ index.js
@@ -250,6 +250,7 @@ app.put("/users/:Username",
   [
     check('Username', 'Username is required').isLength({min: 5}),
     check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required.').not().isEmpty(),
     check('Email', 'Email does not appear to be valid').isEmail()
   ], 
   passport.authenticate("jwt", { session: false }), 
